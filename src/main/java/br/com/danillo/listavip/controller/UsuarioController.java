ad458365e5041d4832e6a45ef3452e0e827bb72e
Danillo <danillo.yamagush@gmail.com>
2019-05-22 10:45:33 -0300

    Mudança Controller

--- src/main/java/br/com/danillo/listavip/controller/UsuarioController.java
+++ src/main/java/br/com/danillo/listavip/controller/UsuarioController.java
@@ -1,11 +1,27 @@
 package br.com.danillo.listavip.controller;
 
+import br.com.danillo.listavip.entity.Usuario;
 import br.com.danillo.listavip.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UsuarioController {
     @Autowired
     UsuarioService usuarioService;
+
+    @RequestMapping(value = "/usuario", method = RequestMethod.GET)
+    public List<Usuario> listUsuario(){
+        return this.usuarioService.listaUsuario();
+    }
+
+    @RequestMapping(value = "/usuario", method = RequestMethod.POST)
+    public List<Usuario> listUsuario(@RequestBody Usuario usuario){
+        return this.usuarioService.listaUsuario(usuario);
+    }
 }
